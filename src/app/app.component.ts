ee38e88ea3bcbabdf8f59be358340b79ae9c7c8b
Gianni Santucci <gianni.d.santucci@gmail.com>
2022-09-07 14:57:54 -0400

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product} from './products.model';
 //Decorador indicando el template, css
 @Component({
   selector: 'app-root',
@@ -16,6 +17,52 @@ export class AppComponent {
     age:18,
     avatar:this.img,
   }
+  /*
+  *  Para practicar ngFor
+  */
+  names: string[] =["Licett","ALan","Gianni"];
+  newName:string="";
+  /*
+  *
+  */
+
+  /*
+  *  Ngfor para Objetos: Tipandolos con una interface->products.model-ts
+  */
+     products: Product[] = [
+    {
+      name: 'El mejor juguete',
+      price: 565,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/glasses.jpg'
+    }
+  ]
+  /***/
 
   //Trabajaando con eventos para el event bingding
   toggleButton(){
@@ -35,5 +82,14 @@ export class AppComponent {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
   }
-  
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName ="";
+  }
+
+  deleteName(index:number){
+    this.names.splice(index,1);
+  }
+
 }
